58d89fdfdd22f68ed3a339f5518347617ac48a1e
sadaan <ansarisadaan72@gmail.com>
2023-08-14 07:15:35 +0530

    added some routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+/* eslint-disable react/jsx-no-undef */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import Home from './components/views/Home';
+import Signin from './components/views/Signin';
+import Signup from './components/views/Signup';
+import Profile from './components/views/Profile';
 
 const App = () => {
   return (
-    <>
-      <h1>From the frontend</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
