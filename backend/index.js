4d3132e92a4865e67b80ccc24a8a0bb18d82691a
sadaan <ansarisadaan72@gmail.com>
2023-08-23 19:01:05 +0530

    added cors validation

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,13 @@ import routerAuth from './routes/auth.js';
 import routerPost from './routes/post.js';
 import routerUser from './routes/user.js';
 
-
+const corsOptions = {
+    origin: true,
+    credentials: true,
+};
 const PORT = 5000;
 dotenv.config()
-app.use(cors());
+app.use('*', cors(corsOptions));
 app.use(express.json());
 app.use(routerAuth);
 app.use(routerPost);
