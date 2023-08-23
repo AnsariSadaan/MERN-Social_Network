d379d7b050ff09613647fc410275a29b8d784914
Ansari Sadaan <76692909+AnsariSadaan@users.noreply.github.com>
2023-08-23 18:46:57 +0530

    Update index.js
    changes in cors for deployment

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ import routerUser from './routes/user.js';
 
 const PORT = 5000;
 dotenv.config()
-app.use(cors());
+app.use(cors(
+ {
+   origin: ["http://deploy-mern-1whq.vercel.app"],
+   methods: ["POST", "GET"],
+   credentials: true
+ }
+));
 app.use(express.json());
 app.use(routerAuth);
 app.use(routerPost);
